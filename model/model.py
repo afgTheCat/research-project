da080ff04ca5f884cb70253387ee3bf0f3cdb754
Gabor Feher <afg@zeroitlab.com>
2022-09-12 11:08:30 +0200

    model progress

---
+++ model/model.py
@@ -0,0 +1,106 @@
+from reservoire import Reservoire
+from sklearn.linear_model import Ridge
+import scipy.io
+import numpy as np
+from sklearn.metrics import accuracy_score, f1_score
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.neural_network import MLPClassifier
+
+
+def compute_test_scores(pred_class, Yte):
+    """
+    Wrapper to compute classification accuracy and F1 score
+    """
+    true_class = np.argmax(Yte, axis=1)
+    accuracy = accuracy_score(true_class, pred_class)
+    if Yte.shape[1] > 2:
+        f1 = f1_score(true_class, pred_class, average="weighted")
+    else:
+        f1 = f1_score(true_class, pred_class, average="binary")
+
+    return accuracy, f1
+
+
+class RCModel:
+    def __init__(
+        self,
+        n_internal_units=100,
+        connectivity=1,
+        izhikevich_params=[
+            0.02,
+            0.2,
+            -65,
+            8,
+        ],
+        internal_weights=None,
+        w_ridge=5,
+        readout="lin",
+    ):
+        a, b, reset_val, d = izhikevich_params
+        self._reservoir = Reservoire(
+            a=a,
+            b=b,
+            reset_val=reset_val,
+            d=d,
+            n_internal_units=n_internal_units,
+            internal_weights=internal_weights,
+            connectivity=connectivity,
+        )
+        if readout == "lin":
+            self.readout = Ridge(alpha=w_ridge)
+        elif readout == "mlp":
+            self.readout = self.readout = MLPClassifier(
+                hidden_layer_sizes=(20, 10),  # this can change
+                activation="relu",
+                # alpha=w_l2,
+                # batch_size=32,
+                learning_rate="adaptive",  # 'constant' or 'adaptive'
+                learning_rate_init=0.001,
+                # max_iter=num_epochs,
+                early_stopping=False,  # if True, set validation_fraction > 0
+                validation_fraction=0.0,  # used for early early_stopping
+            )
+
+    def repr_state(self, res_states):
+        return res_states[:, -1, :]
+
+    def train(self, Xtrain, Y):
+        res_states = self._reservoir.get_states(Xtrain)
+
+        # last state representation
+        input_repr = self.repr_state(res_states)
+
+        # fitting a linear model on both parameters is probably a bad idea, oh well
+        self.readout.fit(input_repr, Y)
+
+    def test(self, Xtest, Yte):
+        res_states = self._reservoir.get_states(Xtest)
+
+        # last state representation
+        input_repr_te = self.repr_state(res_states)
+        logits = self.readout.predict(input_repr_te)
+
+        # Here lies the problem
+        pred_class = np.argmax(logits, axis=1)
+        accuracy, f1 = compute_test_scores(pred_class, Yte)
+        return (accuracy, f1)
+
+
+if __name__ == "__main__":
+    data = scipy.io.loadmat("../data/JpVow.mat")
+
+    Xtrain = data["X"]
+    Ytrain = data["Y"]
+
+    Xtest = data["Xte"]
+    Ytest = data["Yte"]
+
+    onehot_encoder = OneHotEncoder(sparse=False)
+    Ytrain = onehot_encoder.fit_transform(Ytrain)
+    Ytest = onehot_encoder.transform(Ytest)
+
+    rc_model = RCModel(n_internal_units=20)
+    rc_model.train(Xtrain, Ytrain)
+    accuracy, f1 = rc_model.test(Xtest, Ytest)
+
+    print("Accuracy = %.3f, F1 = %.3f" % (accuracy, f1))
--- model/reservoire.py
+++ model/reservoire.py
@@ -2,6 +2,7 @@ from scipy import sparse
 import numpy as np
 import scipy.io
 import matplotlib.pyplot as plt
+import random
 
 
 class Reservoire:
@@ -14,6 +15,7 @@ class Reservoire:
         reset_val=-65,
         d=8,
         dt=0.5,
+        internal_weights=None,
     ) -> None:
         self.n_internal_units = n_internal_units
         self.connectivity = connectivity
@@ -22,27 +24,35 @@ class Reservoire:
         self.reset_val = reset_val
         self.d = d
         self.dt = dt
-        self._initialize_internal_weights()
+        self._initialize_internal_weights(internal_weights)
         self.spike_value = 30
 
         # Input weights depend on input size: they are set when data is provided
         self._input_weights = None
+        self.state_matrix = None
 
-    def _initialize_internal_weights(self):
-        n_internal_units = self.n_internal_units
-        connectivity = self.connectivity
-        internal_weights = sparse.rand(
-            n_internal_units, n_internal_units, density=connectivity
-        ).todense()
-        self.internal_weights = internal_weights
+    def _initialize_internal_weights(self, internal_weights=None):
+        if internal_weights is None:
+            n_internal_units = self.n_internal_units
+            connectivity = self.connectivity
+            internal_weights = sparse.rand(
+                n_internal_units, n_internal_units, density=connectivity
+            ).todense()
+            self.internal_weights = internal_weights
+        else:
+            self.internal_weights = internal_weights
+            print(self.internal_weights)
 
-    def sim_neuron(self, u, v, i):
+    def sim_neuron(self, u, v, i, w):
         if v < self.spike_value:
-            dv = 0.04 * v**2 + 5 * v + 140 - u
+            dv = 0.04 * v**2 + 5 * v + 140 - u + w
             du = self.a * (self.b + v - u)
             v_new = v + (dv + i) * self.dt
-            u_new = u + self.dt * du
-            return (v_new, u_new)
+            if v_new > self.spike_value:
+                return (self.reset_val, u + self.d)
+            else:
+                u_new = u + self.dt * du
+                return (v_new, u_new)
         else:
             v_new = self.reset_val
             u_new = u + self.d
@@ -55,15 +65,30 @@ class Reservoire:
             for neuron in range(self.n_internal_units):
                 v = previous_state[n][neuron]
                 u = previous_state[n][self.n_internal_units + neuron]
+                w = sum(
+                    self.internal_weights[j, neuron] * (v - previous_state[n][j])
+                    for j in range(self.n_internal_units)
+                    if j != neuron
+                )
                 input_val = input_matrix[n][neuron]
 
-                v_new, u_new = self.sim_neuron(u, v, input_val)
+                v_new, u_new = self.sim_neuron(u, v, input_val, w)
                 new_state_matrix[n][neuron] = v_new
                 new_state_matrix[n][self.n_internal_units + neuron] = u_new
 
         return new_state_matrix
 
     def get_states(self, X, input_weight="eq"):
+        """
+        The shape of X should be:
+        * N: number of multivariate times series
+        * T: lenght of the MVTS
+        * V: number of variables
+        Should return:
+        * N: same as above
+        * T: same as above
+        * 2 * U: where U is the number of internal units. The dimensionality doubles since we care about both I and U
+        """
         N, T, V = X.shape
 
         if self._input_weights is None and input_weight == "binom":
@@ -74,28 +99,28 @@ class Reservoire:
             self._input_weights = np.ones((self.n_internal_units, V))
 
         initial_state = [
-            *[-70 for _ in range(self.n_internal_units)],
-            *[-14 for _ in range(self.n_internal_units)],
+            *[-70 + random.uniform(-10, 10) for _ in range(self.n_internal_units)],
+            *[-14 + random.uniform(-3, 3) for _ in range(self.n_internal_units)],
         ]
 
         # State can be described with
         previous_state = np.array([initial_state for _ in range(N)])
-        state_matrix = np.empty((N, T, 2 * self.n_internal_units), dtype=float)
+        self.state_matrix = np.empty((N, T, 2 * self.n_internal_units), dtype=float)
 
         for t in range(T):
             current_input = X[:, t, :]  # [N, V]
             input_matrix = self._input_weights.dot(current_input.T).T
             previous_state = self.simulate_network(previous_state, input_matrix)
-            state_matrix[:, t, :] = previous_state
+            self.state_matrix[:, t, :] = previous_state
 
-        return state_matrix
+        return self.state_matrix
 
-
-def plot_n_th(state_matrix, n_th, neuron, time):
-    V = state_matrix[n_th][:, neuron]
-    fig1 = plt.figure()
-    plt.plot(time, V, label="Membrane Potential")[0]
-    plt.show()
+    def plot_n_th(self, state_matrix, n_th, neuron, time):
+        fig1 = plt.figure()
+        for n in neuron:
+            V = state_matrix[n_th][:, n]
+            plt.plot(time, V, label="Membrane Potential")[0]
+        plt.show()
 
 
 if __name__ == "__main__":
@@ -107,8 +132,19 @@ if __name__ == "__main__":
     dt = 0.5
     time_points = 1000
     Xtr = 10 * np.ones((1, time_points, 1))
-    reservoire = Reservoire(n_internal_units=n_internal_units, dt=dt)
-    state_matrix = reservoire.get_states(Xtr)
+    internal_weights = np.array([[0, 0.1], [-0.1, 0]])
+
+    # internal_weights = None
+    reservoire = Reservoire(
+        n_internal_units=n_internal_units,
+        dt=dt,
+        internal_weights=internal_weights,
+    )
+
+    state_matrix = reservoire.get_states(
+        Xtr,
+        input_weight="eq",
+    )
 
     time = np.arange(0, time_points * dt, dt)  # step values [ms]
-    plot_n_th(state_matrix, 0, 0, time)
+    reservoire.plot_n_th(state_matrix, 0, [0, 1], time)
