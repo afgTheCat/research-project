e49d6c903a82ae8d371e0d865c53773b46e4c262
Gabor Feher <ext.gfeher@p019.xyz>
2024-10-28 08:49:44 +0900

    fix: housekeeping

--- crates/reservoire/src/new_model/mod.rs
+++ crates/reservoire/src/new_model/mod.rs
@@ -33,7 +33,7 @@ impl Model {
     }
 
     // dt is in miliseconds
-    fn step(&mut self, mut input: DVector<f64>, dt: f64) {
+    fn step(&mut self, mut input: DVector<f64>, dt: f64) -> DVector<f64> {
         let firings = self
             .v
             .into_iter()
@@ -43,24 +43,30 @@ impl Model {
 
         for i in firings {
             self.v[i] = self.c[i];
-            self.u[i] = self.u[i] + self.d[i];
+            self.u[i] += self.d[i];
             input += self.connections.column(i);
         }
 
         self.v +=
             dt * ((0.04 * &self.v * &self.v + 5. * &self.v - &self.u + input).add_scalar(140.));
         self.u += dt * &self.a * (&self.b * &self.v - &self.u);
+        self.v.clone()
     }
 }
 
 #[cfg(test)]
 mod test {
-    use nalgebra::{DMatrix, DVector};
-
     use super::Model;
+    use nalgebra::{DMatrix, DVector};
+    use std::{error::Error, fs::File, io, process};
+
+    const DATA_LOCATION: &str = "../../data/output.csv";
 
     #[test]
     fn model_test() {
+        let data = File::create(DATA_LOCATION).unwrap();
+        let mut wtr = csv::Writer::from_writer(data);
+
         let a = DVector::from_vec(vec![0.02]);
         let b = DVector::from_vec(vec![0.2]);
         let c = DVector::from_vec(vec![-65.]);
@@ -73,9 +79,16 @@ mod test {
         let mut model = Model::new(a, b, c, d, v, u, connections);
         let mut voltages = vec![];
 
-        for _ in 0..10000 {
-            model.step(input.clone(), 1.);
-            voltages.push(model.v[0]);
+        // in miliseconds
+        for t in 0..10000 {
+            let voltage = model.step(input.clone(), 1.);
+            voltages.push([t.to_string(), voltage[0].to_string()]);
         }
+
+        for rec in voltages {
+            wtr.write_record(rec).unwrap();
+        }
+
+        wtr.flush().unwrap();
     }
 }
---
+++ scripts/plot_data.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+import csv
+
+DATA_FILE = "../data/output.csv"
+
+
+def show_izhikevich():
+    f = open(DATA_FILE)
+    reader = csv.reader(f)
+
+    data = [(float(d[0]), float(d[1])) for d in reader]
+    d = [d[0] for d in data]
+    r = [d[1] for d in data]
+
+    plt.plot(d, r)
+    plt.show()
+
+
+show_izhikevich()
+
